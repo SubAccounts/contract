504cb1d35822015360cfa04b69bee5992b5dbc3b
dmitrytarassov <dm.tarassov@yandex.ru>
2024-08-22 18:41:13 +0300

    update

--- contracts/SubWallets.sol
+++ contracts/SubWallets.sol
@@ -1,55 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract SecureAccounts {
-    struct Account {
+contract SubWallets {
+    struct SubWallet {
         address owner;
         string encryptedAccount;
         string network;
+        string accountAddress;
     }
 
-    Account[] public accounts;
+    SubWallet[] public subWallets;
 
-    event AccountStored(address indexed owner, string encryptedAccount, string network);
+    event SubWalletStored(
+        address indexed owner,
+        string encryptedAccount,
+        string network,
+        string accountAddress
+    );
 
     function storeEncryptedAccount(
         string memory _encryptedAccount,
-        string memory _network
+        string memory _network,
+        string memory _accountAddress
     ) public {
         // Сохраняем адрес отправителя (msg.sender) и зашифрованное сообщение
-        accounts.push(Account({
+        subWallets.push(SubWallet({
             owner: msg.sender,
             encryptedAccount: _encryptedAccount,
-            network: _network
+            network: _network,
+            accountAddress: _accountAddress
         }));
 
-        emit AccountStored(msg.sender, _encryptedAccount, _network);
+        emit SubWalletStored(msg.sender, _encryptedAccount, _network, _accountAddress);
     }
 
     // Функция для получения всех сохраненных аккаунтов
-    function getAccounts() public view returns (Account[] memory) {
-        return accounts;
+    function getAccounts() public view returns (SubWallet[] memory) {
+        return subWallets;
     }
 
     // Новый метод для получения всех структур по указанному адресу
-    function getAccountsByAddress(address _owner) public view returns (Account[] memory) {
+    function getAccountsByAddress(address _owner) public view returns (SubWallet[] memory) {
         uint count = 0;
 
         // Подсчитываем количество аккаунтов, принадлежащих указанному адресу
-        for (uint i = 0; i < accounts.length; i++) {
-            if (accounts[i].owner == _owner) {
+        for (uint i = 0; i < subWallets.length; i++) {
+            if (subWallets[i].owner == _owner) {
                 count++;
             }
         }
 
         // Создаем массив для хранения результатов
-        Account[] memory result = new Account[](count);
+        SubWallet[] memory result = new SubWallet[](count);
         uint index = 0;
 
         // Заполняем массив результатами
-        for (uint i = 0; i < accounts.length; i++) {
-            if (accounts[i].owner == _owner) {
-                result[index] = accounts[i];
+        for (uint i = 0; i < subWallets.length; i++) {
+            if (subWallets[i].owner == _owner) {
+                result[index] = subWallets[i];
                 index++;
             }
         }
